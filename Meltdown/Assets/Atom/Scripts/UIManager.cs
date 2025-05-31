95d9374053e029d0983c9356d6e4ea2c89537b10
HHoàng <63275057+19127406@users.noreply.github.com>
2025-05-31 10:14:55 +0700

    fix level 14 not load

--- Meltdown/Assets/Atom/Scripts/UIManager.cs
+++ Meltdown/Assets/Atom/Scripts/UIManager.cs
@@ -47,7 +47,7 @@ public class UIManager : MonoBehaviour
     public void LoadLevel(int levelIndex)
     {
         string sceneName = string.Empty;
-        if (levelIndex <= 9 && levelIndex > 13)
+        if (levelIndex <= 9 || levelIndex > 13)
             sceneName = "Hung Level " + levelIndex;
         else
             sceneName = "Hoang Level " + levelIndex;
